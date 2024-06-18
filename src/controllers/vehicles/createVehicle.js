8c1b7225595c84ccefb388f2f7b4181111c949f4
braianbascu <braianbascu@gmail.com>
2024-06-18 11:17:26 -0300

    correciones crear vehiculo

--- src/controllers/vehicles/createVehicle.js
+++ src/controllers/vehicles/createVehicle.js
@@ -3,42 +3,34 @@ import Car from '../../modelos/vehicles.js';
 const createVehicle = async (req, res) => {
   try {
     const {
-      marca,
-      modelo,
-      placa,
-      tipo,
-      capacidad,
-      estado,
-      ubicacionActual,
-      fechaMantenimiento,
+      make,
+      model,
+      plate,
+      type,
+      capacity,
+      status,
+      current_location,
+      maintenance_date,
     } = req.body;
-    if (
-      !(
-        marca ||
-        modelo ||
-        placa ||
-        tipo ||
-        capacidad ||
-        estado ||
-        ubicacionActual ||
-        fechaMantenimiento
-      )
-    )
-      throw Error('Falta informacion necesaria');
 
-    const vehicleLicensePlate = await Car.findOne({ placa });
+    if (!make || !model || !plate || !type || !capacity || !status || !current_location || !maintenance_date) {
+      throw Error('Falta informacion necesaria');
+    }
+     
+
+    const vehicleLicensePlate = await Car.findOne({ plate});
     if (vehicleLicensePlate)
       throw Error('Existe un vehiculo con esa patente asignada');
 
     await Car.create({
-      marca,
-      modelo,
-      placa,
-      tipo,
-      capacidad,
-      estado,
-      ubicacionActual,
-      fechaMantenimiento,
+      make,
+      model,
+      plate,
+      type,
+      capacity,
+      status,
+      current_location,
+      maintenance_date,
     });
 
     return res.status(201).json({ message: 'Vehiculo creado con exito' });
